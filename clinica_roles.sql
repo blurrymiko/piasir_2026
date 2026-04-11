bc5ffa3984664a9783c3cf27702b1dfc433cb3a8
rodrigo <blurrymiko@outlook.com>
2026-04-11 18:14:25 +0200

    Create clinica_roles.sql

---
+++ clinica_roles.sql
@@ -0,0 +1,5 @@
+CREATE ROLE admin WITH LOGIN PASSWORD 'admin_password_segura';
+
+CREATE ROLE consultas WITH LOGIN PASSWORD 'consultas_password_segura';
+
+CREATE ROLE replica WITH LOGIN REPLICATION PASSWORD 'replica_password_segura';
