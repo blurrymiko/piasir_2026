d2d6790b42a4672bef355662dd6bc4f9fee4921b
rodrigo <blurrymiko@outlook.com>
2026-04-12 01:26:19 +0200

    Delete clinica_roles.sql

--- clinica_roles.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE admin WITH LOGIN PASSWORD 'admin_password_segura';
-
-CREATE ROLE consultas WITH LOGIN PASSWORD 'consultas_password_segura';
-
-CREATE ROLE replica WITH LOGIN REPLICATION PASSWORD 'replica_password_segura';
