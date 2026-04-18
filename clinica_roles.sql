e1a3664c90dc9284896e3babf771531a244af466
rodrigo <blurrymiko@outlook.com>
2026-04-18 18:33:25 +0200

    Update clinica_roles.sql

--- clinica_roles.sql
+++ clinica_roles.sql
@@ -1,39 +1,42 @@
 -- Roles y privilegios SQL: BBDD clinica_privada
 
-CREATE ROLE admin NOLOGIN;
+CREATE ROLE dba NOLOGIN;
 CREATE ROLE consultas NOLOGIN;
 CREATE ROLE escritura NOLOGIN;
-
-CREATE ROLE u_admin WITH LOGIN PASSWORD 'C2GnFn@v9M8.';
-CREATE ROLE u_consultas WITH LOGIN PASSWORD '4WL6kcY9-kjE';
-CREATE ROLE u_escritura WITH LOGIN PASSWORD '4WL6kcY9-kjE';
 CREATE ROLE replica WITH LOGIN REPLICATION PASSWORD 'C2GnFn@v9M8.';
 
-GRANT admin TO u_admin;
-GRANT consultas TO u_consultas;
-GRANT escritura TO u_escritura;
+CREATE ROLE dba1 WITH LOGIN PASSWORD 'C2GnFn@v9M8.';
+CREATE ROLE tecnico1 WITH LOGIN PASSWORD '4WL6kcY9-kjE';
+CREATE ROLE trabajador1 WITH LOGIN PASSWORD '4WL6kcY9-kjE';
 
--- Tras crear la BBDD
-GRANT CONNECT ON DATABASE clinica_privada TO u_admin, u_escritura, u_consultas;
+GRANT dba TO dba1;
+GRANT consultas TO tecnico1;
+GRANT escritura TO trabajador1;
 
--- Como u_admin
-GRANT USAGE ON SCHEMA public TO admin, escritura, consultas;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO admin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO escritura;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO consultas;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO admin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO escritura;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO consultas;
+-- Creamos la base de datos 
+-- CREATE DATABASE clinica_privada OWNER dba1;
+GRANT CONNECT ON DATABASE clinica_privada TO dba, consultas, escritura;
 
--- Tras importar el esquema (como u_admin)
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
-GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO escritura;
+-- Trabajamos ya como dba1
+-- psql -U dba1 -d clinica_privada o \c clinica_privada SET ROLE dba1;
+GRANT USAGE ON SCHEMA public TO consultas, escritura;
+GRANT CREATE ON SCHEMA public TO dba;
+
+-- Importamos la estructura (clinica_schema)
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO consultas;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
-GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO escritura;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO consultas;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO escritura;
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO escritura;
 
--- Índices, datos (como u_admin)
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO consultas;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO consultas;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO escritura;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO escritura;
+
+-- Importamos índices (clinica_index) y registros (clinica_registros)
+
+
+
 
 
 
